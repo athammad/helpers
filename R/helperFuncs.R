edf874af1fefada58da125e98771c82367d327cf
Ahmed T. Hammad <37300344+athammad@users.noreply.github.com>
2022-10-19 08:25:54 +0200

    added plotGPS func

--- R/helperFuncs.R
+++ R/helperFuncs.R
@@ -50,3 +50,46 @@ setnames(mergedData,newNames)
 }
 return(mergedData)
 }
+                    
+                    
+
+plotGPS<-function(dt=NULL,LAT="Latitude", LON="Longitude",...){
+  
+  
+  #Get the world map country border points
+  library(maps)
+  library(ggplot2)
+  world_map <- map_data("world")
+  
+  
+  #Creat a base plot with gpplot2
+  p <- ggplot() + coord_fixed() +
+    xlab("") + ylab("")
+  
+  #Add map to base plot
+  base_world_messy <- p + geom_polygon(data=world_map, aes(x=long, y=lat, group=group), 
+                                       colour="darkolivegreen3", fill="darkolivegreen3")
+  
+  
+  
+  
+  cleanup <- 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          panel.background = element_rect(fill = 'white', colour = 'white'), 
+          axis.line = element_line(colour = "white"), legend.position="none",
+          axis.ticks=element_blank(), axis.text.x=element_blank(),
+          axis.text.y=element_blank())
+  
+  base_world <- base_world_messy + cleanup
+  
+  
+  map_data <- 
+    base_world +
+    geom_point(data=dt, 
+               aes_string(x=LON, y=LAT), colour="Blue", 
+               fill="Light Blue",pch=21, size=5, alpha=I(0.7))
+  
+  return(map_data)
+  
+  
+}
