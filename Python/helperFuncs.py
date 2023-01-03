35dcb09f827118ba56aa58b39e2e4d512d6b0ad8
Ahmed T. Hammad <37300344+athammad@users.noreply.github.com>
2023-01-03 05:57:36 +0100

    added plot and installling functions

--- Python/helperFuncs.py
+++ Python/helperFuncs.py
@@ -20,3 +20,95 @@ def df_optimized(df, verbose=True, **kwargs):
         print("optimized size by {} % | {} GB".format(ratio, GB))
     return df
 
+##############################################################################################
+
+import subprocess
+import sys
+
+def install_packages(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    
+#install_packages("fitter")
+
+
+def installed_packages(package):
+    result=package in sys.modules
+    print(result)
+    
+#installed_packages("distributions")
+
+
+
+###################################################################################
+import scipy.stats as ss
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn') # pretty matplotlib plots
+plt.rcParams['figure.figsize'] = (12, 8)
+
+
+def plot_normal(x_range=[0, 1], mu=0, sigma=1, cdf=False, **kwargs):
+    '''
+    Plots the normal distribution function for a given x range
+    If mu and sigma are not provided, standard normal is plotted
+    If cdf=True cumulative distribution is plotted
+    Passes any keyword arguments to matplotlib plot function
+    '''
+    x = x = np.linspace(x_range[0], x_range[1], 5000)
+    if cdf:
+        y = ss.norm.cdf(x, mu, sigma)
+    else:
+        y = ss.norm.pdf(x, mu, sigma)
+    plt.plot(x, y, **kwargs)
+    
+
+plot_normal(mu=0.5, sigma=0.1)
+
+
+def plot_normal(x_range=[0, 1], mu=0.5, sigma=0.1, cdf=False, **kwargs):
+    '''
+    Plots the normal distribution function for a given x range
+    If mu and sigma are not provided, standard normal is plotted
+    If cdf=True cumulative distribution is plotted
+    Passes any keyword arguments to matplotlib plot function
+    '''
+    x = x = np.linspace(x_range[0], x_range[1], 5000)
+    if cdf:
+        y = ss.norm.cdf(x, mu, sigma)
+    else:
+        y = ss.norm.pdf(x, mu, sigma)
+    plt.plot(x, y, **kwargs)
+    
+
+#plot_normal(mu=0.5, sigma=0.1)
+
+
+
+
+import tensorflow_probability as tfp
+import numpy as np
+import matplotlib.pyplot as plt
+tfd = tfp.distributions
+plt.style.use('seaborn') # pretty matplotlib plots
+plt.rcParams['figure.figsize'] = (12, 8)
+
+def plot_sinar(x_range=[0, 1], mu=0.5, sigma=0.1,skewness = 1.2,tailweight=0, cdf=False, **kwargs):
+    '''
+    Plots the SinhArcsinh distribution function for a given x range
+    If mu and sigma are not provided, default distribution paramters are used
+    If cdf=True cumulative distribution is plotted
+    Passes any keyword arguments to matplotlib plot function
+    '''
+    sinar=tfd.SinhArcsinh(mu, sigma, skewness, tailweight)
+    x = x = np.linspace(x_range[0], x_range[1], 5000)
+    if cdf:
+        y = sinar.cdf(x)
+    else:
+        y = sinar.prob(x)
+    plt.plot(x, y, **kwargs)
+    
+#mu= 0.2,sigma = 0.2,,nu = 1.2, tau = 0.1
+#plot_sinar(mu= 0.1,sigma = 0.6,skewness=1.2,tailweight=2,cdf=False)
+
+
